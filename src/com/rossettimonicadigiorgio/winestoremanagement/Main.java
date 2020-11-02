40f32667b459479ddf633243667367d87692440a
Francesco Rossetti <16597707+fraross@users.noreply.github.com>
2020-11-02 15:44:07 +0100

    02112020

---
+++ src/com/rossettimonicadigiorgio/winestoremanagement/Main.java
@@ -0,0 +1,17 @@
+package com.rossettimonicadigiorgio.winestoremanagement;
+
+/**
+ * @author 296666
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
